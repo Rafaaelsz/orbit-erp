3fd235ae5c60044d9cb2ff69e0920ba59cd9eef8
Rafaaelsz <rafaeltperaldo@gmail.com>
2025-11-21 09:10:44 -0300

    Fix: Adicionando barra no STATIC_URL

--- setup/settings.py
+++ setup/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,7 +90,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WHITENOISE_USE_FINDERS = True
